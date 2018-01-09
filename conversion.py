0401d572a66c6eecee78f993f343b72a8de9625f
Kevin Bird <kbird@illinois.edu>
2018-01-09 11:25:38 -0600

    Adding gallons2liters function

--- conversion.py
+++ conversion.py
@@ -1,1 +1,9 @@
-kbird made this
+# My Conversion Tools
+
+def dollars2cents(dollars):
+    cents = dollars * 100
+    return cents
+
+def gallons2liters(gallons):
+    liters = gallons * 3.78541
+    return liters
